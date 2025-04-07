40f5f79898bb4a191e93a1a57b56b766ba3d1f83
Ra.khaihan <rakharaihan2244@gmail.com>
2025-04-07 17:20:13 +0700

    menambahkan class DataMahasiswa103022300046

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿namespace tpmodul7_kelompok_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa103022300046 dataRakha = new DataMahasiswa103022300046();
+            dataRakha.ReadJSON();
+
+            
+        }
+    }
+}
