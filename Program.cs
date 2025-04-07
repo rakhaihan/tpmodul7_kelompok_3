205744592ff404d3539ba90700de83520d34728c
adiityanugraha <150656990+adiityanugraha@users.noreply.github.com>
2025-04-07 17:37:56 +0700

    Add files via upload
    Menambahkan Class DataMahasiswa_103022300063 dan KuliahMahasiswa_103022300063

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿// See https://aka.ms/new-console-template for more information
+namespace tpmodul7_kelompok_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa_103022300063 dataAgung = new DataMahasiswa_103022300063();
+            dataAgung.ReadJSON();
+            KuliahMahasiswa_103022300063 kuliahAgung = new KuliahMahasiswa_103022300063();
+            kuliahAgung.ReadJSON();
+        }
+    }
+}
