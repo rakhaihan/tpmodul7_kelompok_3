ac6d7d35fcc3df0cd0c71feba0fb006d7871fe3e
Zipooxy <142865402+Zipooxy@users.noreply.github.com>
2025-04-07 17:34:30 +0700

    Add files via upload
    menambahkan class DataMahasiswa dan KuliahMahasiswa

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿namespace tpmodul7_kelompok_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa_103022300024 dataJack = new DataMahasiswa_103022300024();
+            dataJack.ReadJSON();
+
+            KuliahMahasiswa_103022300024 kuliahJack = new KuliahMahasiswa_103022300024();
+            kuliahJack.ReadJSON();
+        }
+    }
+}
